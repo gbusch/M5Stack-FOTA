eb83ee678fab325c64281db331ca5b0b5d92f55d
Gerold Busch <gerold.busch@gmail.com>
2020-11-12 17:02:45 +0100

    test automatic update

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
 
-#define VERSION 2
+#define VERSION 3
 
 void setup() {
 
@@ -22,7 +22,6 @@ void loop() {
     M5.Lcd.setCursor(10, 10);
     M5.Lcd.setTextColor(WHITE);
     M5.Lcd.setTextSize(1);
-    M5.Lcd.printf("Display Test!");
     M5.Lcd.fillTriangle(random(M5.Lcd.width()-1), random(M5.Lcd.height()-1), random(M5.Lcd.width()-1), random(M5.Lcd.height()-1), random(M5.Lcd.width()-1), random(M5.Lcd.height()-1), random(0xfffe));
     M5.update();
 
@@ -45,8 +44,6 @@ void loop() {
     }
     // Free resources
     http.end();
-
-    M5.Lcd.println(payload);
 
     JSONVar myObject = JSON.parse(payload);
   
